234be129a040199ebfc493c7d99d593fe4d8043a
Ahasan Ullah <ahasanullah451@gmail.com>
2025-09-22 18:35:01 +0600

    user repo created in the DAl

---
+++ DAL/Repos/UserRepo.cs
@@ -0,0 +1,45 @@
+﻿using DAL.EF;
+using DAL.EF.Tables;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos
+{
+    internal class UserRepo:IRepo<User,int,bool>
+    {
+        MarketContext db;
+        public UserRepo()
+        {
+            db =new MarketContext();
+        }
+
+        public bool Create(User obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int iD)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(User obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
