df2031ed8f2956249c8bfd9919097c409cb56145
Ahasan Ullah <ahasanullah451@gmail.com>
2025-09-22 17:09:37 +0600

    random users added to the Users table

--- DAL/Migrations/Configuration.cs
+++ DAL/Migrations/Configuration.cs
@@ -4,6 +4,7 @@
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using System.Reflection;
 
     internal sealed class Configuration : DbMigrationsConfiguration<DAL.EF.MarketContext>
     {
@@ -18,6 +19,19 @@
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
+
+
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    context.Users.AddOrUpdate(new EF.Tables.User
+            //    {
+            //        Name = "Name" + i,
+            //        Password = "Password" + i,
+            //        Role = i % 2 == 0 ? "Admin" : "User",
+            //        Address = "Address " + i,
+            //        CreatedAt = DateTime.Now.AddDays(-i)
+            //    });
+            //}
         }
     }
 }
