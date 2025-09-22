74e29098c27743556c88aa43e60e1a680c2ce634
Ahasan Ullah <ahasanullah451@gmail.com>
2025-09-22 16:06:19 +0600

    IRepo interface added for dependency injection

---
+++ DAL/Interfaces/IRepo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IRepo<CLASS,ID,RET>
+    {
+        RET Create(CLASS obj);
+      
+        RET Update(CLASS obj);
+
+        List<CLASS> Get();
+
+        CLASS Get(ID id);
+
+        bool Delete(ID iD);
+    }
+}
