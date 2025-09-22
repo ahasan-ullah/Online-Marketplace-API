c8752b25cd0564acd49873d45a9099aee22ba9c4
Ahasan Ullah <ahasanullah451@gmail.com>
2025-09-22 14:05:04 +0600

    migration added but user table not created

--- DAL/EF/MarketContext.cs
+++ DAL/EF/MarketContext.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using DAL.EF.Tables;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DAL.EF
 {
-    internal class UMSContext
+    public class MarketContext:DbContext
     {
+        public DbSet<User> Users;
     }
 }
