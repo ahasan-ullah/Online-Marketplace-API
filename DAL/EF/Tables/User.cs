9e31b22a1c5a30adb4ca6db251f0ffebf8481517
Ahasan Ullah <ahasanullah451@gmail.com>
2025-09-22 13:48:28 +0600

    model and context added for user in the DAL also added the connectionstring to the presentation layer

---
+++ DAL/EF/Tables/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EF.Tables
+{
+    public class User
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Password { get; set; }
+        [Required]
+        public string Role { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; }
+    }
+}
