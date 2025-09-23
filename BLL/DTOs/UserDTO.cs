10227718982078a547eb379622432ccd17ca9021
Ahasan Ullah <ahasanullah451@gmail.com>
2025-09-23 10:49:27 +0600

    DTO Created in the BLL and automapper v10.0 installed

---
+++ BLL/DTOs/UserDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTOs
+{
+    public class UserDTO
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public string Address { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
