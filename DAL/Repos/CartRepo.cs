9679cf6d5f52207b28a52606398c2c757c9fa1cd
Ahasan Ullah <ahasanullah451@gmail.com>
2025-10-06 16:55:36 +0600

    cart repo created

---
+++ DAL/Repos/CartRepo.cs
@@ -0,0 +1,44 @@
+﻿using DAL.EF;
+using DAL.EF.Tables;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos
+{
+    internal class CartRepo : IRepo<Cart, int, bool>
+    {
+        MarketContext db;
+        public CartRepo()
+        {
+            db=new MarketContext();
+        }
+        public bool Create(Cart obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Cart> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cart Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Cart obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
